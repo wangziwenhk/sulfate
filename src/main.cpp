443ba77ee5cc78695f898f33e255bc1091740f06
wangziwenhk <wangziwen2011@icloud.com>
2025-02-24 21:49:45 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include "stdint.h"
+#include "stddef.h"
+#include <limine.h>
+
+__attribute__((used, section(".limine_requests")))
+static volatile LIMINE_BASE_REVISION(3)
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile LIMINE_REQUESTS_START_MARKER
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile LIMINE_REQUESTS_END_MARKER
+
+
+extern "C"
+[[noreturn]] void kmain(void) {
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        for (;;) __asm__ ("hlt");
+    }
+
+    if (framebuffer_request.response == nullptr
+        || framebuffer_request.response->framebuffer_count < 1) {
+        for (;;) __asm__ ("hlt");
+    }
+
+    limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    uint64_t width = framebuffer->width;
+    uint64_t height = framebuffer->height;
+
+    uint64_t stride = framebuffer->pitch / 4;
+    uint32_t *buffer = static_cast<uint32_t *>(framebuffer->address);
+
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            buffer[y * stride + x] = (x * 255 / width) << 16 | (y * 255 / height) << 8;
+        }
+    }
+
+    for (;;) __asm__ ("hlt");
+}
---
+++ xmake.lua
@@ -0,0 +1,67 @@
+set_project("ExampleOS")
+
+add_rules("mode.debug", "mode.release")
+add_requires("llvm")
+
+set_arch("x86_64")
+set_optimize("fastest")
+
+set_policy("run.autobuild", true)
+set_policy("check.auto_ignore_flags", false)
+
+add_cxflags("-target x86_64-freestanding")
+add_ldflags("-target x86_64-freestanding")
+
+add_cxflags("-mno-80387", "-mno-mmx", "-mno-sse", "-mno-sse2", "-msoft-float","-nostdinc")
+add_cxflags("-mcmodel=kernel")
+add_ldflags("-static","-nostdlib")
+
+add_cxflags("-mno-red-zone", "-fno-exceptions", "-fno-rtti","-m64")
+
+target("kernel")
+    set_kind("binary")
+    set_languages("c++20")
+    set_toolchains("llvm")
+
+    set_default(false)
+
+    add_files("src/**.cpp")
+    add_includedirs("include")
+    add_ldflags("-T assets/linker.ld", "-e kmain")
+
+target("iso")
+    set_kind("phony")
+    add_deps("kernel")
+    set_default(true)
+
+    on_build(function (target)
+        import("core.project.project")
+
+        local iso_dir = "$(buildir)/iso"
+        os.cp("assets/limine/*", iso_dir .. "/limine/")
+
+        local target = project.target("kernel")
+        os.cp(target:targetfile(), iso_dir .. "/kernel.elf")
+
+        local iso_file = "$(buildir)/ExampleOS.iso"
+        os.run("xorriso -as mkisofs -efi-boot-part --efi-boot-image --protective-msdos-label " ..
+                "-no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus "..
+                "-R -r -J -apm-block-size 2048 "..
+                "--efi-boot limine/limine-uefi-cd.bin "..
+                "%s -o %s", iso_dir, iso_file)
+        print("ISO image created at: %s", iso_file)
+    end)
+
+    on_run(function (target)
+        import("core.project.config")
+
+        local flags = {
+            "-M", "q35",
+            "-cpu", "qemu64,+x2apic",
+            "-smp", "4",
+            "-drive", "if=pflash,format=raw,file=assets/ovmf-code.fd",
+            "-cdrom", config.buildir() .. "/ExampleOS.iso"
+        }
+
+        os.runv("qemu-system-x86_64", flags)
+    end)
